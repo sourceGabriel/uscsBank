eb8178b93c79a8407b83c9b5e8ff33e703e26d49
gabriel fonte <fontegabriel@gmail.com>
2020-05-24 00:17:59 -0300

    First commit

---
+++ src/main/java/com/uscs/uscsBank/UscsBankApplication.java
@@ -0,0 +1,13 @@
+package com.uscs.uscsBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UscsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UscsBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uscs/uscsBank/UscsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uscs.uscsBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UscsBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
